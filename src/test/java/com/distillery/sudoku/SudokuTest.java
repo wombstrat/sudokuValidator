00a61276695700a84d21b4caecd4d7e432b565ca
Ramiro Jimenez <ramiro.jimenez@classpass.com>
2020-07-13 17:25:00 -0500

    Adding test for repeated number in subGrid.

--- src/test/java/com/distillery/sudoku/SudokuTest.java
+++ src/test/java/com/distillery/sudoku/SudokuTest.java
@@ -31,6 +31,27 @@ public class SudokuTest {
 
     }
 
+    // Upper-left sub grid contains 5 twice. 0,0 and 2,2
+    @Test
+    public void boardInvalidBcNumberRepeatedIntTheSameSubGrid() {
+        Character[][] board = new Character[][]{
+                {'5', '3', '4', '6', '7', '8', '9', '1', '2'},
+                {'6', '7', '2', '1', '9', '5', '3', '4', '8'},
+                {'1', '9', '5', '3', '4', '2', '5', '6', '7'},
+                {'8', '5', '9', '7', '6', '1', '4', '2', '3'},
+                {'4', '2', '6', '8', '5', '3', '7', '9', '1'},
+                {'7', '1', '3', '9', '2', '4', '8', '5', '6'},
+                {'9', '6', '1', '5', '3', '7', '2', '8', '4'},
+                {'2', '8', '7', '4', '1', '9', '6', '3', '5'},
+                {'3', '4', '5', '2', '8', '6', '1', '7', '9'}
+        };
+
+        printBoard(board);
+        assertFalse(isValidSudoku(board));
+
+
+    }
+
     // 5 at 0,0 is repeated at the same row
     @Test
     public void isBoardInvalidBcOfRow() {
